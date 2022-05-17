1a836c20cfbaf60a7b0518a99a5ef48c5a2360f2
bharadwajpudi <bharadwajpudi2018@gmail.com>
2022-05-17 10:30:56 +0530

    SpringbootValidation done

---
+++ springbootvalidationday43/src/main/java/com/jobiak/imd/Springbootvalidationday43Application.java
@@ -0,0 +1,13 @@
+package com.jobiak.imd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootvalidationday43Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootvalidationday43Application.class, args);
+	}
+
+}
---
+++ springbootvalidationday43/src/main/java/com/jobiak/imd/model/SignupBean.java
@@ -0,0 +1,46 @@
+package com.jobiak.imd.model;
+
+import java.io.Serializable;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class SignupBean implements Serializable {
+	
+	@NotEmpty(message="cant leave the field empty")
+	@Size(min=10,max=10,message="have to enter 10 digits")
+	@Valid
+	private String mobile;
+	
+	@NotBlank(message="Can't leave field blank")
+	@Valid
+	@Email
+	private String email;
+	
+	@NotEmpty(message="Can't leave the field empty")
+	@Size(min=12,max=12,message="have to enter all 12 digits")
+	private String aadhar;
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAadhar() {
+		return aadhar;
+	}
+	public void setAadhar(String aadhar) {
+		this.aadhar = aadhar;
+	}
+	
+	
+}
